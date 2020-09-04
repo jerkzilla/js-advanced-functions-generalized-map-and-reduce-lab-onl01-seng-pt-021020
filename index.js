b354e3766ecd4f355ff15a6de93369c1f880be4f
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-09-04 15:04:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(arr, func) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i ++) {
+        let el = arr[i];
+        newArr.push(func(el));
+    }
+
+    return newArr;
+}
+
+function reduce(arr, func, start) {
+    let val = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+
+    for (; i < arr.length; i++) {
+        val = func(arr[i], val)
+    }
+
+    return val;
+}
